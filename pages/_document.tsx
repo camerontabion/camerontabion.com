ab4b0ac14153c0b535a0f4e77f29173a442cef95
Cameron Keokolo Tabion <cameronkeokolotabion@gmail.com>
2021-08-06 20:32:18 -0700

    Setup app

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="description" content="Cameron Keokolo Tabion's personal portfolio website." />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/icon.png"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
