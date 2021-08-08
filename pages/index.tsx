2b02635475d790fb7a559c77509f4d9029616ad1
Cameron Keokolo Tabion <cameronkeokolotabion@gmail.com>
2021-08-07 19:13:52 -0700

    Organized page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import Head from 'next/head';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Projects from '../components/Projects';
+import Footer from '../components/Footer';
 
 const Home = () => (
   <>
     <Head>
       <title>Cameron Keokolo Tabion</title>
     </Head>
+    <Hero />
+    <About />
+    <Projects />
+    <Footer />
   </>
 );
 
