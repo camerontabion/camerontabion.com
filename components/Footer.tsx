4f07c11aea35ca5296080e35407aaa2537b2b4bc
Cameron Keokolo Tabion <cameronkeokolotabion@gmail.com>
2021-08-07 19:13:39 -0700

    Added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <p>
+      &copy; 2021 Cameron Keokolo Tabion
+    </p>
+  </footer>
+);
+
+export default Footer;
