11eb875225420635c304e48d69c5a847083e13d5
Cameron Keokolo Tabion <cameronkeokolotabion@gmail.com>
2021-08-20 15:05:48 -0700

    Fixed typo

--- data/about.ts
+++ data/about.ts
@@ -2,5 +2,5 @@ export const title = 'Hello, I\'m Cameron!';
 
 export const paragraphs = [
   'I am a full stack developer with a passion for creating clean and effective web apps using modern technologies.',
-  'Currently, I am finishing my degree in Computer Science at the University of Nevada, Las Vegas, and am working part-time at UNLV’s DataX Lab under Dr. Mingon Kang.',
+  'Currently, I am finishing my degree in Computer Science at the University of Nevada, Las Vegas, and working part-time at UNLV’s DataX Lab under Dr. Mingon Kang.',
 ];
