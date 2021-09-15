f2ab555ac6ebcd9cc044cc2ebdcc3a5ce7c32d84
Cameron Keokolo Tabion <cameronkeokolotabion@gmail.com>
2021-09-15 14:17:08 -0700

    Added projects

--- data/projects.ts
+++ data/projects.ts
@@ -1,3 +1,5 @@
+// https://lvsp.camerontabion.com/
+
 export const projects = [
   {
     name: 'Healage',
@@ -6,6 +8,24 @@ export const projects = [
     links: [
       { name: 'Website Repo', href: 'https://github.com/camerontabion/healage-website' },
       { name: 'Server Repo', href: 'https://github.com/camerontabion/healage-server' },
+    ],
+  },
+  {
+    name: 'Personal Website',
+    description: 'My personal/portfolio website showcasing my skills and projects.',
+    image: '/camerontabion.png',
+    links: [
+      { name: 'View Website', href: 'https://www.camerontabion.com' },
+      { name: 'GitHub Repo', href: 'https://github.com/camerontabion/camerontabion.com' },
+    ],
+  },
+  {
+    name: 'Official LVSP Website',
+    description: 'The official website for the Las Vegas Scholars Program.',
+    image: '/lvsp.png',
+    links: [
+      { name: 'View Website', href: 'https://lvsp.camerontabion.com' },
+      { name: 'GitHub Repo', href: 'https://github.com/camerontabion/lvsp-website' },
     ],
   },
   {
