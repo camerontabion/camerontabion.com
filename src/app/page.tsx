1ff687b4a331858b9f024fb668da3e34eb96e795
Cameron Keokolo Tabion <cameronkeokolotabion@gmail.com>
2025-06-24 04:16:39 -0700

    added a fade effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Skills from "./Skills";
 export default function App() {
   return (
     <main className="relative flex w-full flex-col gap-24 overflow-hidden p-8 text-white md:py-24">
+      <div className="pointer-events-none absolute inset-0 z-10 size-full animate-fade-out bg-black" />
       <div className="-z-10 absolute inset-0 size-full animate-pulse-background bg-[url('/star.png')] bg-center bg-size-[48px] bg-repeat" />
       <Hero />
       <Projects />
